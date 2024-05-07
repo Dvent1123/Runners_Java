26882defdd7cb49975e0bb75b59baab6b7d852c5
Dvent1123 <ventura.daniel36@gmail.com>
2024-05-06 19:59:15 -0700

    load json data

---
+++ src/main/java/ventura/daniel/runnerz/run/RunJsonDataLoader.java
@@ -0,0 +1,20 @@
+package ventura.daniel.runnerz.run;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+
+public class RunJsonDataLoader implements CommandLineRunner {
+	
+	private static final Logger log = LoggerFactory.getLogger(RunJsonDataLoader.class);
+	private final RunRepository runRepository;
+	
+	public RunJsonDataLoader(RunRepository runRepository) {
+		this.runRepository = runRepository;
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+	}
+}
